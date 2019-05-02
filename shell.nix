3883e3348eb1799e1116e06418e5d67c343d5ad6
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-05-02 20:13:16 +0200

    Split status-go Nix package per platform so rebuilds are faster

--- shell.nix
+++ shell.nix
@@ -4,11 +4,7 @@ with pkgs;
 
 let
   projectDeps = import ./default.nix { inherit target-os; };
-  targetMobile = {
-    "android" = true;
-    "ios" = true;
-    "all" = true;
-  }.${target-os} or false;
+  platform = callPackage ./nix/platform.nix { inherit target-os; };
   # TODO: Try to use stdenv for iOS. The problem is with building iOS as the build is trying to pass parameters to Apple's ld that are meant for GNU's ld (e.g. -dynamiclib)
   _stdenv = stdenvNoCC;
   _mkShell = mkShell.override { stdenv = _stdenv; };
@@ -25,7 +21,7 @@ in _mkShell {
     ps # used in scripts/start-react-native.sh
     unzip
     wget
-  ] ++ lib.optionals targetMobile [ bundler ruby ]; # bundler/ruby used for fastlane
+  ] ++ lib.optionals platform.targetMobile [ bundler ruby ]; # bundler/ruby used for fastlane
   inputsFrom = [ projectDeps ];
   TARGET_OS=target-os;
   shellHook =
